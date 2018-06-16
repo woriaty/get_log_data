b3ddf4fcab06a49f68e0f592838d3d8d738fada1
woriaty <1546168053@qq.com>
2018-06-16 23:30:55 +0800

    Change find string method.

--- get_time.py
+++ get_time.py
@@ -1,31 +1,30 @@
 #coding=utf-8
 f = open("out.csv","w")
-f.write('start time' + ',' + 'end time' + '\n')  #write title
-data = ""
-time = {'start':'','end':''}
-num = 0
-start = 0
-end = 0
-for line in open("test.txt","r"):
-    for i in range(len(line)):
-        if line[i:i+10] == "start time":
-            start = i+11
-            i = start
-        if line[i:i+8] == "end time":
-            start = i+9
-            i = start
-        if line[i] == ',' and start != 0:
-            end = i
-            data = line[start:end]
-            if num == 0:
-                time['start'] = data
-                f.write(time['start'] + ',')
-                print("start_time = %s" % time['start'])
-                num += 1
-            else:
-                time['end'] = data
-                f.write(time['end'] + ',' + '\n')
-                print("end_time = %s" % time['end'])
-                num = 0
-            start = 0
-f.close()
+f.write('num' + ',' + 'start time' + ',' + 'end time' + '\n')  #write title
+
+def find_match(line, string):
+    match = ''
+    pos = line.index(string) + len(string)
+    for i in range(pos, len(line)):
+        if line[i] == ',':
+            break;
+        match += line[i]
+    return match
+
+def get_time(file_name):
+    num = 0
+    for line in open(file_name + ".log","r"):
+        num += 1
+        try:
+            start_time = find_match(line, "start time")
+            print("start time = %s" % start_time)
+            end_time = find_match(line, "end time")
+            print("end time = %s" % end_time)
+        except ValueError:
+            num -= 1
+            continue
+        f.write(str(num) + ',' + start_time + ',' + end_time + ',' + '\n')
+    f.close()
+
+if __name__ == "__main__":
+    get_time("test")
